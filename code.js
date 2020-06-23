9c5c9ff314194a768f9bcaf090831b47737cf32a
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-06-23 16:00:44 -0500

    learning tests in js

--- code.js
+++ code.js
@@ -5,14 +5,20 @@ function helloWorld() {
 
 // sayHello function
 function sayHello(input) {
-    // if (input === "Alex") {
-    //     return "Hello, Alex!";
-    // }
-    // else if (input === "Pat") {
-    //     return "Hello, Pat!";
-    // }
-    // else {
-    //     return "Hello, Jane!";
-    // }
-    return "Hello, " + input + "!"
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    }
+    else if (input === "Pat") {
+        return "Hello, Pat!";
+    }
+    else if (input === true) {
+        return "Hello, World!";
+    }
+    else if (input === false) {
+        return "Hello, World!";
+    }
+    else {
+        return "Hello, Jane!";
+    }
+    // return "Hello, " + input + "!"
 }
--- tests.js
+++ tests.js
@@ -30,4 +30,10 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!" when executed', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 });
