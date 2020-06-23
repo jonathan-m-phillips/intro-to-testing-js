75cffafb788e931d4d81b38229087b1c8f7a9e8c
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-06-23 15:47:38 -0500

    learning tests on js

--- code.js
+++ code.js
@@ -2,3 +2,17 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+// sayHello function
+function sayHello(input) {
+    // if (input === "Alex") {
+    //     return "Hello, Alex!";
+    // }
+    // else if (input === "Pat") {
+    //     return "Hello, Pat!";
+    // }
+    // else {
+    //     return "Hello, Jane!";
+    // }
+    return "Hello, " + input + "!"
+}
