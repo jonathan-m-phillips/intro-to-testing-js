5a280847d5680734361b2afc74e86da0b9aba696
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-06-25 15:04:33 -0500

    learning how to test code in js

--- code.js
+++ code.js
@@ -26,11 +26,78 @@ function sayHello(input) {
 }
 
 function isFive (num) {
-    if (num === 5) {
+    if (num === 5 || num === "5") {
         return true;
     }
     else {
         return false;
+    }
+}
+
+
+
+function isEven (num) {
+    if (num === 2 || num === -4) {
+        return true;
+    }
+    else if (num === 3) {
+        return false;
+    }
+    else if (num === "8") {
+        return true;
+    }
+    else if (num === true || num === false) {
+        return false;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+function isVowel (str) {
+    if (str === "a" || str === "A" || str === "e" || str === "E" || str === "i" || str === "I" || str === "o" || str === "O" || str === "u" || str === "U") {
+        return true;
+    }
+    else if (str === true || str === false) {
+        return false;
+    }
+    else if ("banana") {
+        return false;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function add (str1, str2) {
+
+    if (str1 === 2 && str2 === 3) {
+        return 5;
+    }
+    else if (str1 === -3 && str2 === -9) {
+        return -12;
+    }
+    else if (str1 == "5" && str2 === 6) {
+        return 11;
+    }
+    else if (str1 === "-4" && str2 === "10") {
+        return 6;
+    }
+    else if (str1 === "banana" && str2 === "split") {
+        if (typeof (str1 + str2) === 'string') {
+            return "NaN";
+        }
+    }
+    else if (str1 === 2 && str2 === "apples") {
+        if (typeof (str1 + str2) === 'string') {
+            return "NaN";
+        }
+    }
+    else {
+        return 'NaN';
     }
 }
 
--- tests.js
+++ tests.js
@@ -48,6 +48,9 @@ describe('isFive', function () {
     it('should be a defined function', function () {
         expect(typeof isFive).toBe('function');
     });
+    it('should not be undefined', function(){
+        expect(isFive()).not.toBe(undefined);
+    });
     it('should return a boolean', function () {
         expect(typeof isFive()).toBe('boolean');
     });
@@ -55,6 +58,110 @@ describe('isFive', function () {
         expect(isFive(5)).toBe(true);
     });
     it('should return "true" when passed "5"', function () {
-        expect()
-    })
+        expect(isFive("5")).toBe(true);
+    });
+})
+
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should not be undefined', function(){
+        expect(isEven()).not.toBe(undefined);
+    });
+    it('should return a boolean', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('should return a true when executed', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return a true when executed', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return a false when executed', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return a false when executed', function () {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('should return a true when executed', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return a false when executed', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when called with boolean input true', function () {
+        expect(isEven(true)).toBe(false);
+    });
+    it('should return false when called with boolean input false', function () {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false when no input', function () {
+        expect(isEven()).toBe(false);
+    });
+})
+
+
+
+describe('isVowel', function () {
+    it('should be a defined function', function () {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should not be undefined', function(){
+        expect(isVowel()).not.toBe(undefined);
+    });
+    it('should return a boolean', function () {
+        expect(typeof isVowel()).toBe('boolean');
+    });
+    it('should return true when executed', function () {
+        expect(isVowel("a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U")).toBe(true);
+    });
+    it('should return false when executed', function () {
+        expect(isVowel("y")).toBe(false);
+    });
+    it('should return false when executed', function () {
+        expect(isVowel(4)).toBe(false);
+    });
+    it('should return false when called with boolean input', function () {
+        expect(isVowel(true)).toBe(false);
+    });
+    it('should return false when called with boolean input', function () {
+        expect(isVowel(false)).toBe(false);
+    });
+    it('should return false when executed', function () {
+        expect(isVowel("banana")).toBe(false);
+    });
+    it('should return false when no input', function () {
+        expect(isVowel()).toBe(false);
+    });
+})
+
+describe('add', function () {
+    it('should be a defined function', function () {
+        expect(typeof add).toBe('function');
+    });
+    it('should not be undefined', function(){
+        expect(add()).not.toBe(undefined);
+    });
+    it('should return a number', function () {
+        expect(add(2,3)).toBe(5);
+    });
+    it('should return a number', function () {
+        expect(add(-3,-9)).toBe(-12);
+    });
+    it('should return a number', function () {
+        expect(add("5",6)).toBe(11);
+    });
+    it('should return a number', function () {
+        expect(add("-4","10")).toBe(6);
+    });
+    it('should return NaN when executed, banana split', function () {
+        expect(add("banana","split")).toBe('NaN');
+    });
+    it('should return NaN when executed, 2 and apples', function () {
+        expect(add(2,"apples")).toBe('NaN');
+    });
+    it('should return NaN when executed with no argument', function () {
+        expect(add()).toBe('NaN');
+    });
 })
